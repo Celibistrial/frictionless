d3c3f1f8cd918caa9b584ab7b3d9f881fa5020e7
Celibistrial <ryan80222@gmail.com>
2021-04-20 14:47:43 +0530

    Adding Commands

--- src/main/java/frictionless/frictionless/Frictionless.java
+++ src/main/java/frictionless/frictionless/Frictionless.java
@@ -1,12 +1,37 @@
 package frictionless.frictionless;
-
+// getString(ctx, "string")
+import static com.mojang.brigadier.arguments.StringArgumentType.getString;
+// word()
+import static com.mojang.brigadier.arguments.StringArgumentType.word;
+// literal("foo")
+import static net.minecraft.server.command.CommandManager.literal;
+// argument("bar", word())
+import static net.minecraft.server.command.CommandManager.argument;
+// Import everything
+import static net.minecraft.server.command.CommandManager.*;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.command.v1.CommandRegistrationCallback;
+import net.minecraft.block.dispenser.FallibleItemDispenserBehavior;
 import net.minecraft.client.options.KeyBinding;
+import net.minecraft.server.command.CommandManager;
 
-public class Frictionless implements ModInitializer {
+
+
+public class Frictionless implements ModInitializer{
+    public static boolean isSlip;
 
     @Override
     public void onInitialize() {
-
+        CommandRegistrationCallback.EVENT.register((dispatcher, dedicated) -> {
+            dispatcher.register(literal("toggleSlip").executes(context -> {
+                if(isSlip){
+                    isSlip = false;
+                }
+                else if(!isSlip){
+                    isSlip = true;
+                }
+                return 1;
+            }));
+        });
     }
 }
--- src/main/java/frictionless/frictionless/mixin/slippymixin.java
+++ src/main/java/frictionless/frictionless/mixin/slippymixin.java
@@ -1,19 +1,54 @@
 package frictionless.frictionless.mixin;
 
+import frictionless.frictionless.Frictionless;
+import net.fabricmc.fabric.api.command.v1.CommandRegistrationCallback;
 import net.minecraft.block.Block;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import frictionless.frictionless.Frictionless;
+import static net.minecraft.server.command.CommandManager.literal;
+
 
 @Mixin(Block.class)
 public class slippymixin {
+
+
+   public float slip = 1F;
+   //def is 0.6
+   public float vel = 1.05F;
+   //def is 1
+
+
+
     @Inject(method = "getSlipperiness", at = @At(value = "HEAD"), cancellable = true)
     private void slippyFix(CallbackInfoReturnable<Float> cir) {
-        cir.setReturnValue(1F);
+        if(Frictionless.isSlip){
+            slip = 1F;
+
+        }else if(!Frictionless.isSlip){
+            slip = 0.6F;
+        }
+        cir.setReturnValue(slip);
+
     }
+
+
+
     @Inject(method = "getVelocityMultiplier", at = @At(value = "HEAD"), cancellable = true)
     private void velFix(CallbackInfoReturnable<Float> cir) {
-        cir.setReturnValue(1.05F);
+        if(Frictionless.isSlip){
+            vel = 1.05F;
+
+        }else if(!Frictionless.isSlip){
+            vel =1F;
+        }
+        cir.setReturnValue(vel);
     }
+
+
+
 }
+
+
