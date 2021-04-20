28cbe75f210e82073775caf98e78dfd0ba360c66
Celibistrial <ryan80222@gmail.com>
2021-04-20 11:39:01 +0530

    Added Keybinds

--- src/main/java/frictionless/frictionless/Frictionless.java
+++ src/main/java/frictionless/frictionless/Frictionless.java
@@ -1,8 +1,10 @@
 package frictionless.frictionless;
 
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.client.options.KeyBinding;
 
 public class Frictionless implements ModInitializer {
+
     @Override
     public void onInitialize() {
 
